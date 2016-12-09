b954563738bed4065c39fe1f0d452778948f0f37
Neeraj Shah <neerajvshah1@gmail.com>
2016-12-08 22:40:22 -0800

    Added javascript functionality. Random quote getter is done (for now at least)

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-<script>
-	$(document).ready(function(){
-		$("#generate").on("click",function(){
-			
-		});
+
+$(document).ready(function(){
+  getQuoteSetText();
+	$("#generate").on("click",function(){
+    getQuoteSetText();
 	});
-</script>
+});
+
+function getQuoteSetText(){
+  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function (data){
+	console.log(data["quoteText"]);
+	console.log(data["quoteAuthor"]);
+    $("#quote_text").text(data["quoteText"]);
+    $("#quote_author").text(data["quoteAuthor"]);
+  });	
+}
